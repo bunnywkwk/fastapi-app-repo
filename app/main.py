5b9b4f2f80b9cc34efb6913628f03b82dcd9b467
bunnywkwk <aeronsolano06@gmail.com>
2026-08-03 08:42:50 +0800

    change new version into 6

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ def read_root():
     return {
         "app_name": settings.APP_NAME,
         "environment": settings.ENVIRONMENT,
-        "status": "healthy v5.0"
+        "status": "healthy v6.0"
     }
 
 
