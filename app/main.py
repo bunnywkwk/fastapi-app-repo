a5f759fc8b34dee1fadf089c635f297d4f5474f1
bunnywkwk <aeronsolano06@gmail.com>
2026-08-03 09:11:08 +0800

    feat: upgrade to v8.0

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ def read_root():
     return {
         "app_name": settings.APP_NAME,
         "environment": settings.ENVIRONMENT,
-        "status": "healthy v7.0"
+        "status": "healthy v8.0"
     }
 
 
